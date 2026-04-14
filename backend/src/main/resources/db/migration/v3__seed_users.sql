786e6559cf1a649ce3faa8a006a1f94b13e4b13d
M4STERPIECEE <michaelramana2021@gmail.com>
2026-04-14 14:13:53 +0300

    fix: update placeholder text for username input in login form

--- backend/src/main/resources/db/migration/v3__seed_users.sql
+++ backend/src/main/resources/db/migration/v3__seed_users.sql
@@ -1,4 +1,4 @@
 INSERT INTO app_user (username, password, role) VALUES
                                                     ('MASTERPIECE', 'zntPEGASUS77K', 'ADMIN'),
-                                                    ('COMEBACK', 'POULET123', 'USER')
+                                                    ('COMEBACK', ' ', 'USER')
 ON CONFLICT (username) DO NOTHING;
